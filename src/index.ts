f1e6a183fe5f0df10cb98f3ef7f455701c585c9c
Nobrain11 <chainpulseink@gmail.com>
2026-08-03 15:57:33 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { registerWelcome } from "./welcome.js";
 import { registerAiChat } from "./aiChat.js";
 import { registerModeration } from "./moderation.js";
 import { registerAdminCommands } from "./adminCommands.js";
+import { registerMenu } from "./menu.js";
 
 const bot = new Bot(config.botToken);
 
@@ -12,13 +13,27 @@ registerCommands(bot);
 registerAdminCommands(bot);
 registerWelcome(bot);
 registerModeration(bot);
+registerMenu(bot);
 registerAiChat(bot);
 
 bot.catch((err) => {
   console.error("Unhandled bot error:", err.error);
 });
 
-bot.start({
-  allowed_updates: ["message", "chat_member"],
-  onStart: (info) => console.log(`RobinBot online as @${info.username}`),
-});
+async function launch() {
+  while (true) {
+    try {
+      await bot.api.deleteWebhook({ drop_pending_updates: true });
+      await bot.start({
+        allowed_updates: ["message", "chat_member"],
+        onStart: (info) => console.log(`RobinBot online as @${info.username}`),
+      });
+      break;
+    } catch (err) {
+      console.error("Bot loop crashed, restarting in 5s:", err);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+  }
+}
+
+launch();
